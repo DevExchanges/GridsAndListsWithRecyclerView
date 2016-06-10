3941a1ac2a6637d763c570087499a24a528afd04
DevExchanges <wandered.vip.007@gmail.com>
2016-06-10 11:45:07 +0700

    update click event

--- app/src/main/java/info/devexchanges/recyclerview/HorizontalListAdapter.java
+++ app/src/main/java/info/devexchanges/recyclerview/HorizontalListAdapter.java
@@ -2,16 +2,11 @@ package info.devexchanges.recyclerview;
 
 import android.app.Activity;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 import android.widget.Toast;
-
-import java.util.List;
 
 public class HorizontalListAdapter extends RecyclerView.Adapter<HorizontalListAdapter.ViewHolder> {
 
